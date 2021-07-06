56ab569549e7fae1da38be709acefa5ab5f3dc6e
youssef aouadni <75804874+youssefaouadni@users.noreply.github.com>
2021-07-06 14:32:08 +0100

    File Upload-download

---
+++ choice.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p><h1>chooce an operation to proceed</h1></p>
+
+    <p> <a href="file-upload-download/index.php?upload your files ='1'" style="color: green;">upload your files</a> </p>
+    <p> <a href="file-upload-download/downloads.php?download your files ='1'" style="color: green;">download your files</a> </p>
+
+  </body>
+</html>
